04e230f66128a40bf8fb354d929640d98fde5e5f
Min Cheol Kim <kmc724@zinggls.com>
2023-07-28 15:45:45 +0900

    Send rotation value

--- NUCLEO-F412ZG/Stepper/tk/main.py
+++ NUCLEO-F412ZG/Stepper/tk/main.py
@@ -1,19 +1,33 @@
 import tkinter
+import serial
 
 class Main:
     def __init__(self):
         self.root = tkinter.Tk()
-    
+        self.uart = serial.Serial("/dev/ttyACM0", 115200)
+        
         lbl = tkinter.Label(self.root, text = "Send UART Message")
         btn = tkinter.Button(self.root, text = "Send", command = self.btn_callback)
         lbl.grid(row = 0, column = 0)
-        btn.grid(row = 0, column = 1)
+        vcmd = self.root.register(self.entry_validate)
+        self.ent = tkinter.Entry(self.root, validate = "all", validatecommand = (vcmd, "%P"))
+        self.ent.grid(row = 1, column = 0)
+        btn.grid(row = 1, column = 1)
 
     def run(self):
         self.root.mainloop()
 
     def btn_callback(self):
-        print("Callback")
+        if (self.ent.get() != ""):
+            rot = self.ent.get()
+            send = [0x07, 0x00, int(rot), 0x24]
+            self.uart.write(send)
+
+    def entry_validate(self, i):
+        if str.isdigit(i) or i == "":
+            return True
+        else:
+            return False
 
 if __name__ == "__main__":
     main = Main()
